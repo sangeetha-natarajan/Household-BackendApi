05dc92a8aa616ae540039e2e058ff8dfd666c934
Sangeetha Natarajan <nbsangee@gmail.com>
2023-09-09 20:20:50 +0800

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+// load env vars
+dotenv.config({path:'./config/config.env'})
+
+const app =express()
+const PORT = process.env.PORT || 5000
+const ENV = process.env.NODE_ENV 
+
+app.listen( PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`))
